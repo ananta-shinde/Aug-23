24ee4d674d1fdac5bb981ee142121d4aaea36458
aShinde <ananta.shinde@honeywellcom>
2024-01-12 09:04:57 +0530

    12/01/24

--- revision/oop02.cpp
+++ revision/oop02.cpp
@@ -1,34 +1,56 @@
 #include<stdio.h>
-
+#include<string.h>
 class Person{
-   
+    protected:
     char name[20];
     int uid;
     char bloodgroup[3];
+    public:
+    void setName(char * name)
+    {
+        strcpy(this->name,name);
+    }
+    void demo()
+    {
+         printf("\nThis is base class method");
+    }
+    void demo(int a)
+    {
+         printf("\nThis is overloaded base class method");
+    }
 };
 
-class Teacher: Person {
+class Teacher:public Person {
     public:
     int id;
+    void demo()
+    {
+         printf("\nThis is teacher class method");
+    }
+    
 };
 
-class Student :Person{
-     
+class Student :public Person{
+     public:
+     int rollNo;
+    
 };
 
 int main(int argc, char const *argv[])
 {
-    Teacher t3;
-    Teacher* t1 = new Teacher();
-    t1->id = 200;
-    Teacher* t2 = new Teacher();
-    t2->id = 250;
-    t3.id = 300;
+    Person* person = new Person();
+    //  Teacher* teacher = new Teacher();
+    //  Student* student = new Student();
+    //  teacher->setName("Ananta");
+    //  student->setName("Ananta");
+    //  student->rollNo = 20;
 
-    t1 = t2;
-
-    printf("%d\n",t3.id);
-    printf("%d\n",t1->id);
-    printf("%d",t2->id);
+    person->demo();
+    person = new Teacher();
+    person->demo(20);
+    Teacher* teacher = new Teacher();
+    teacher->demo();
+      
+     
     return 0;
 }
